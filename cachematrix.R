9cc098ad8208c99159110d62b22d83f104cbd2d8
RagdeNas <edgar.sanchez.me@gmail.com>
2018-02-24 20:03:59 -0600

    MWeek3 v1.0
    24.02.2018
    
    .Creation of Programming Assignment function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions that cache the calculation of the inverse of a matrix
 
-## Write a short comment describing this function
+## The following one saves the matrix data and the calculated inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+     x <<- y
+     m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function( solve ) m <<- solve
+  getInverse <- function() m
+  list( set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The following one checks if the matrix's inverse is already in cache, else, performs the solve
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if( !is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve( data,... )
+  x$setInverse(m)
+  m
 }
